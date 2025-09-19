a6b4ae8255922ed9a0e20ea60725e2a3a09198ac
meAltf <alataf.dce.cs1@gmail.com>
2025-09-19 11:30:18 +0530

    implemented search api | mongo template

--- src/main/java/com/learn/springboot_mongodb/controller/PersonController.java
+++ src/main/java/com/learn/springboot_mongodb/controller/PersonController.java
@@ -3,6 +3,9 @@ package com.learn.springboot_mongodb.controller;
 import com.learn.springboot_mongodb.collection.Person;
 import com.learn.springboot_mongodb.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -33,5 +36,18 @@ public class PersonController {
     public List<Person> getPersonByAge(@RequestParam Integer minAge,
                                        @RequestParam Integer maxAge) {
         return personService.getPersonByAge(minAge, maxAge);
+    }
+
+    @GetMapping("/search")
+    public Page<Person> searchPerson(
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) Integer minAge,
+            @RequestParam(required = false) Integer maxAge,
+            @RequestParam(required = false) String city,
+            @RequestParam(defaultValue = "0") Integer page,
+            @RequestParam(defaultValue = "5") Integer size) {
+
+        Pageable pageable = PageRequest.of(page, size);
+        return personService.search(name, minAge, maxAge, city, pageable);
     }
 }
