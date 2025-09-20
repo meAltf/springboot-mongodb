48f9263113042e5daef2690fca035570643859ad
meAltf <alataf.dce.cs1@gmail.com>
2025-09-20 08:26:55 +0530

    created repo, interface, impl

---
+++ src/main/java/com/learn/springboot_mongodb/service/PhotoService.java
@@ -0,0 +1,4 @@
+package com.learn.springboot_mongodb.service;
+
+public interface PhotoService {
+}
---
+++ src/main/java/com/learn/springboot_mongodb/service/impl/PhotoServiceImpl.java
@@ -0,0 +1,8 @@
+package com.learn.springboot_mongodb.service.impl;
+
+import com.learn.springboot_mongodb.service.PhotoService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PhotoServiceImpl implements PhotoService {
+}
