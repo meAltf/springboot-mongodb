c941fd851d707971de342c7a2009dc170e543c18
meAltf <alataf.dce.cs1@gmail.com>
2025-09-20 09:55:24 +0530

    correction-2

--- src/main/java/com/learn/springboot_mongodb/controller/PhotoController.java
+++ src/main/java/com/learn/springboot_mongodb/controller/PhotoController.java
@@ -11,6 +11,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.Encoding;
+
 import java.io.IOException;
 
 @RestController
@@ -20,7 +26,7 @@ public class PhotoController {
     @Autowired
     private PhotoService photoService;
 
-    @PostMapping("/save")
+    @PostMapping(value = "/save", consumes = "multipart/form-data")
     public String addPhoto(@RequestParam("image") MultipartFile image) throws IOException {
         String id = photoService.addPhoto(image.getOriginalFilename(), image);
         return id;
